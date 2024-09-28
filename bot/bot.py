239eba1be64960d4999324ae927efc079f12dcc9
neet-007 <moayedeffect@gmail.com>
2024-09-28 21:56:31 +0300

    added basic error handler that sends exceptions to developer chat in telegram

--- bot/bot.py
+++ bot/bot.py
@@ -1,9 +1,8 @@
-from telegram import Update
 from telegram.ext import Application
 from dotenv import load_dotenv
 from os import getenv
 from sys import exit
-from bot.shared_handlers import RateLimitHandler, chack_rate_limit_handler
+from bot.shared_handlers import RateLimitHandler, chack_rate_limit_handler, error_handler
 from games.draft_handlers import random_team_draft_game_callback_handler, make_game_test_handler, join_game_test_handler, start_game_test_handler, set_game_test_handler, position_draft_message_test_handler, cancel_game_test_handler, vote_recive_poll_answer_test_handler, end_vote_game_test_handler, join_game_callback_test_handler, start_vote_game_test_handler
 from games.guess_the_player_handlers import guess_the_player_start_game_command_handler, guess_the_player_join_game_command_handler, guess_the_player_new_game_command_handler,guess_the_player_ask_question_command_handler, guess_the_player_cancel_game_command_handler, guess_the_player_join_game_callback_handler, guess_the_player_leave_game_command_handler, guess_thE_player_get_questions_command_handler, handle_dispatch_messages, new_db_handler, guess_the_player_dispatch_handler
 
@@ -29,6 +28,7 @@ sync_ptb = (
     .build()
 )
 
+ptb.add_error_handler(error_handler)
 ptb.add_handler(RateLimitHandler(chack_rate_limit_handler))
 ptb.add_handler(new_db_handler)
 #ptb.add_handler(guess_the_player_new_game_command_handler)
--- bot/shared_handlers.py
+++ bot/shared_handlers.py
@@ -1,7 +1,12 @@
+from os import getenv
 from telegram import Update
+from telegram.constants import ParseMode
 from telegram.ext import ContextTypes, BaseHandler
 from db.connection import get_session
 from db.shared_queries import check_rate_limit
+import traceback
+from json import dumps
+from html import escape
 
 def check_rate_limit_function(player_id):
         with get_session() as session:
@@ -11,6 +16,28 @@ def check_rate_limit_function(player_id):
                return False
 
         return True
+
+async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+    DEVELOPER_CHAT_ID = getenv("DEVELOPER_CHAT_ID")
+    if not DEVELOPER_CHAT_ID:
+        return
+
+    tb_list = traceback .format_exception(None, context.error, context.error.__traceback__) # type: ignore
+    tb_string = "".join(tb_list)
+
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    message = (
+        "An exception was raised while handling an update\n"
+        f"<pre>update = {escape(dumps(update_str, indent=2, ensure_ascii=False))}"
+        "</pre>\n\n"
+        f"<pre>context.chat_data = {escape(str(context.chat_data))}</pre>\n\n"
+        f"<pre>context.user_data = {escape(str(context.user_data))}</pre>\n\n"
+        f"<pre>{escape(tb_string)}</pre>"
+    )
+
+    await context.bot.send_message(
+        chat_id=DEVELOPER_CHAT_ID, text=message, parse_mode=ParseMode.HTML
+    )
 
 async def chack_rate_limit_handler(update: Update, context:ContextTypes.DEFAULT_TYPE):
     if not update.effective_user or not update.effective_chat:
